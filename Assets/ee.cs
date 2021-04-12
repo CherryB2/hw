5f82dbc9420357cd8293c02809c25d3992f7c375
CherryB2 <tgbnm7@naver.com>
2021-04-12 16:50:26 +0900

    first update

---
+++ Assets/ee.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ee : MonoBehaviour
+{
+    Animator ani;
+    int a = 0;
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        ani.SetBool("bJump", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a += 1;
+        Debug.Log(a);
+        if(a>500)
+        {
+            ani.SetBool("bJump", true);
+        }
+        else
+        {
+            ani.SetBool("bJump", false);
+        }
+    }
+}
